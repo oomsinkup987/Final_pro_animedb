eced24983f700c8d20e53ea6bdb752ae66d3a617
oomsinkup987 <71169348+oomsinkup987@users.noreply.github.com>
2022-03-17 03:03:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from website import create_app
-
+import os
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(ssl_context= "adhoc")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(ssl_context= "adhoc",host='0.0.0.0', port=port)
